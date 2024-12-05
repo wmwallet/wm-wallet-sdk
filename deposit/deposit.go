0ae897d3100e7e9f558216ec4629f5cb6986d7b2
wm <wm>
2024-12-05 16:32:19 +0800

    add detail

--- deposit/deposit.go
+++ deposit/deposit.go
@@ -33,13 +33,34 @@ type (
 		Url          string          `json:"url"`
 	}
 
+	GetDetailRequest struct {
+		SourceId string `json:"source_id"`
+	}
+
+	GetDetailResponse struct {
+		SourceId     string          `json:"source_id"`
+		ChainId      int             `json:"chain_id"`
+		CoinId       int             `json:"coin_id"`
+		Address      string          `json:"address"`
+		Tag          string          `json:"tag"`
+		Hash         string          `json:"hash"`
+		FiatAmount   decimal.Decimal `json:"fiat_amount"`
+		Symbol       string          `json:"symbol"`
+		ExchangeRate decimal.Decimal `json:"exchange_rate"`
+		Amount       decimal.Decimal `json:"amount"`
+		OrderId      string          `json:"order_id"`
+		Url          string          `json:"url"`
+		Status       int8            `json:"status"`
+		StatusDesc   string          `json:"status_desc"`
+	}
+
 	CancelOrderReq struct {
 		SourceId string `json:"source_id"`
 	}
 
 	CancelOrderResp struct{}
 
-	Resp[T CreateOrderResp | CancelOrderResp] struct {
+	Resp[T CreateOrderResp | GetDetailResponse | CancelOrderResp] struct {
 		Data T      `json:"data"`
 		Code int    `json:"code"`
 		Msg  string `json:"msg"`
@@ -48,6 +69,7 @@ type (
 
 const (
 	routeBrokerOrderCreate = "/v1/api/broker/order/create"
+	routeBrokerOrderDetail = "/v1/api/broker/order/detail"
 	routeBrokerOrderCancel = "/v1/api/broker/order/cancel"
 )
 
@@ -83,6 +105,29 @@ func (d *Deposit) Create(ctx context.Context, req *CreateOrderReq) (*CreateOrder
 	return &resp.Data, nil
 }
 
+func (d *Deposit) Detail(ctx context.Context, req *GetDetailRequest) (*GetDetailResponse, error) {
+	r, err := buildReq(ctx, req, d.url, routeBrokerOrderDetail)
+	if err != nil {
+		return nil, err
+	}
+	body, err := d.w.Post(ctx, r)
+	if err != nil {
+		return nil, err
+	}
+	var tmp string
+	if err := json.Unmarshal(body, &tmp); err != nil {
+		return nil, err
+	}
+	var resp = &Resp[GetDetailResponse]{}
+	if err := json.Unmarshal([]byte(tmp), &resp); err != nil {
+		return nil, err
+	}
+	if err = getErr(resp); err != nil {
+		return nil, err
+	}
+	return &resp.Data, nil
+}
+
 func (d *Deposit) Cancel(ctx context.Context, req *CancelOrderReq) (*CancelOrderResp, error) {
 	r, err := buildReq(ctx, req, d.url, routeBrokerOrderCancel)
 	if err != nil {
@@ -107,7 +152,7 @@ func (d *Deposit) Cancel(ctx context.Context, req *CancelOrderReq) (*CancelOrder
 	return &resp.Data, nil
 }
 
-func buildReq[T *CreateOrderReq | *CancelOrderReq](ctx context.Context, req T, baseUrl, router string) (*http.Request, error) {
+func buildReq[T *CreateOrderReq | *GetDetailRequest | *CancelOrderReq](ctx context.Context, req T, baseUrl, router string) (*http.Request, error) {
 	body, err := json.Marshal(req)
 	if err != nil {
 		return nil, err
@@ -120,7 +165,7 @@ func buildReq[T *CreateOrderReq | *CancelOrderReq](ctx context.Context, req T, b
 	return r, nil
 }
 
-func getErr[T CreateOrderResp | CancelOrderResp](resp *Resp[T]) error {
+func getErr[T CreateOrderResp | GetDetailResponse | CancelOrderResp](resp *Resp[T]) error {
 	if resp.Code == 0 {
 		return nil
 	}
